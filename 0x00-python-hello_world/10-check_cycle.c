25f4d0f9a1b91c9f1e1bf43635608f522a58f56f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-09 23:34:44 +0000

    condensed code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,23 +14,15 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow;
-	listint_t *fast;
-
-/*	if (list == NULL)
-	return (0);*/
-
-	slow = list;
-	fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	while (fast != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
-		{
 			return (1);
-		}
 	}
 	return (0);
 }
