21f828ccc18177e144964cb2ee747f6e939df5e8
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-09 19:36:00 +0000

    input answers

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import write from sys
+str = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+write(stderr, str, 1)
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+Learn and practice the basics of Python
