764abdcf3685c1028d036efe05f840d6696f2498
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-09 23:44:22 +0000

    removed extra libraries

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,15 +1,10 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
  * check_cycle - function that finds a loop in a linked list
  * based on Floyd’s cycle-finding algorithm
  * @list: pointer to the struct listint_t
- *
  * Return: 0 if there is no cycle, 1 if there is a cycle
- *
  */
 
 int check_cycle(listint_t *list)
