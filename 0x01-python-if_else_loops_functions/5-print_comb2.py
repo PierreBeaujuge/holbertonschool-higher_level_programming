9b181802eb59c88bdf1c5b85298aa0dbea1a50f6
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-11 02:52:50 +0000

    switched from str(i) to i and printed as numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print('0' + '{}'.format(str(i)), end=', ')
-    elif i >= 10 and i < 99:
-        print('{}'.format(str(i)), end=', ')
+    if i < 99:
+        print('{:02d}'.format(i), end=', ')
     else:
-        print('{}'.format(str(i)))
+        print('{:02d}'.format(i))
