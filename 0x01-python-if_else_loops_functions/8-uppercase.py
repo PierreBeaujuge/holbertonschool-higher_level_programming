7705fb56e0641498bfd95edcfcce6cb19ef4ec6a
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-11 01:59:35 +0000

    added format as per guidelines and added blank line before def

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+
 def uppercase(str):
     for char in str[0:-1]:
         if char >= 'a' and char < '{':
             char = chr(ord(char) - 32)
-        print(char, end='')
+        print('{}'.format(char), end='')
     if str[-1] >= 'a' and str[-1] < '{':
         char = chr(ord(str[-1]) - 32)
-    print(char)
+    print('{}'.format(char))
