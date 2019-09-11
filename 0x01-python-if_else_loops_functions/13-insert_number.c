6ba84ade67cabcce449bd8d2b334872ed43d1353
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-11 03:25:46 +0000

    add if not(head)

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -22,6 +22,9 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	new_node->next = NULL;
 
+	if (!head)
+		return (NULL);
+
 	if (current == NULL)
 	{
 		if (number > prev->n)
