e4461899fdd989cf4dfd00fb5ac0af778ffd34ca
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-12 21:39:58 +0000

    swicthed to imported functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
         exit(1)
 
     if sys.argv[2] == '+':
-        result = int(sys.argv[1]) + int(sys.argv[3])
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
     elif sys.argv[2] == '-':
-        result = int(sys.argv[1]) - int(sys.argv[3])
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
     elif sys.argv[2] == '*':
-        result = int(sys.argv[1]) * int(sys.argv[3])
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
     elif sys.argv[2] == '/':
-        result = int(sys.argv[1]) / int(sys.argv[3])
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
     print('{:d} {} {:d} = {:d}'.format(int(sys.argv[1]), sys.argv[2],
                                        int(sys.argv[3]), result))
