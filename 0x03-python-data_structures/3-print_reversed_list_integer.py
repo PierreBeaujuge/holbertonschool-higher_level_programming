641a85b2fd79ff141f11995991ebc1fa0509b6cf
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-17 23:49:27 +0000

    return empty string with no newline

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,9 +4,10 @@
 def print_reversed_list_integer(my_list=[]):
 
     if len(my_list) == 0:
-        return None
-    my_list = reversed(my_list)
-    for num in my_list:
-        print('{:d}'.format(num))
+        print('', end='')
+    else:
+        my_list = reversed(my_list)
+        for num in my_list:
+            print('{:d}'.format(num))
 
 #    return print('{}'.format(my_list))
