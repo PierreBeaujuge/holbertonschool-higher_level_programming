0b6b1fecd1d345d714d22d96ae049b2842c589c9
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-17 23:34:28 +0000

    code based on new_string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,8 @@
 
 def no_c(my_string):
 
-    my_string = my_string.replace('c', '')
-    my_string = my_string.replace('C', '')
-    return my_string
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
