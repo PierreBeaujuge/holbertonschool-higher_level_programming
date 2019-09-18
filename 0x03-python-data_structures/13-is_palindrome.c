d681dfbe0d60c0412b6f4eaff5d4556638239e9d
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-18 05:00:07 +0000

    betty compliant

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 
 /**
- *
+ * is_palindrome - function that checks if a singly linked list is a palindrome
+ * @head: double pointer, pointer to pointer to head defined in main
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 
 int is_palindrome(listint_t **head)
@@ -16,7 +18,6 @@ int is_palindrome(listint_t **head)
 		return (0);
 	if (!(*head))
 		return (1);
-
 	temp = *head;
 	while (temp != NULL)
 		temp = temp->next, /*count++*/i++;
@@ -25,34 +26,19 @@ int is_palindrome(listint_t **head)
 	array = malloc(sizeof(int) * ((len / 2) + 1));
 	if (array == NULL)
 		return (-1);
-	i = 0;
-	temp = *head;
+	i = 0, temp = *head;
 	while (i < (len / 2))
 	{
 		array[i] = temp->n;
-/*		printf("temp->n = %i\n", temp->n);
-		printf("array[i] = %i\n", array[i]);*/
 		temp = temp->next, i++;
 	}
-/*	i = 0;
-	while (i < (len / 2))
-	printf("array[i] = %i\n", array[i]), i++;*/
 	i = 0;
 	if (len % 2 != 0)
 		temp = temp->next;
-	while(temp != NULL)
+	while (temp != NULL)
 	{
-/*		printf("i = %i\n", i);
-		printf("(len / 2) - i = %i\n", (len / 2) - i);
-		printf("temp->n = %i\n", temp->n);
-		printf("array[i] = %i\n", array[((len / 2) - 1) - i]);*/
-/*		if (len % 2 != 0)
-		temp = temp->next;*/
-/*		printf("temp->n = %i\n", temp->n);*/
 		if (temp->n == array[((len / 2) - 1) - i])
 		{
-/*			printf("temp->n = %i\n", temp->n);
-			printf("array[i] = %i\n", array[i]);*/
 			temp = temp->next, i++;
 			continue;
 		}
