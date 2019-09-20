8c9c0ea63013753c99a1aa16ddfc95ed36987757
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-20 04:32:46 +0000

    switched to if a_dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@
 
 def best_score(a_dictionary):
 
-#    if a_dictionary is not None and a_dictionary != {}:
     if a_dictionary:
         sorted_dict = sorted(a_dictionary.items(), key=lambda item: item[1])
         return (sorted_dict[-1])[0]
