5c0921097f5c765b861c056a309e47e202081f41
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-20 03:18:15 +0000

    updated condition on dictionary not None and != {}

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary and a_dictionary != {}:
+    if a_dictionary is not None and a_dictionary != {}:
         if len(a_dictionary) > 1:
             sorted(a_dictionary.items(), key=lambda item: item[1])
         return list(a_dictionary.keys())[-1]
