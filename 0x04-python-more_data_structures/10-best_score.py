f96dc95dc2f2fc80b61da6f6371907cca775a40b
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-20 03:31:12 +0000

    removed return for single element dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,6 @@ def best_score(a_dictionary):
     if a_dictionary is not None and a_dictionary != {}:
         if len(a_dictionary) > 1:
             sorted(a_dictionary.items(), key=lambda item: item[1])
-            return list(a_dictionary.keys())[-1]
-        return list(a_dictionary.keys())[0]
+        return list(a_dictionary.keys())[-1]
     else:
         return None
