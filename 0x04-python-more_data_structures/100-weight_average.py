85a104f66c1ddccee540792693a8fe090ccedf67
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-09-20 05:54:43 +0000

    input answer

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+
+    dot_product = 0
+    add_values = 0
+    if my_list:
+        for element in my_list:
+            dot_product += element[0] * element[1]
+            add_values += element[1]
+        weight_average = dot_product / add_values
+        return weight_average
+    else:
+        return 0
