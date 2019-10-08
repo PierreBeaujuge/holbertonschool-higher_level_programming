e4bdf52f5a4dd38aeaaba0ea15b808bf2f2f1695
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-10-08 10:48:47 +0000

    input answers

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Rectangle management module
+"""
+
+
+class Rectangle:
+    """define variables and methods"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Learn and practice More Classes and Objects
