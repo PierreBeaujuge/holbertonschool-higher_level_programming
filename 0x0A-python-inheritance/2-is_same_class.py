1316ea0087a19c8b19c9911190991e9407f74d77
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-10-16 04:39:50 +0000

    input answers

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+is_same_class module
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function that returns True if the object is exactly an instance
+    of the specified class ; otherwise False
+    """
+    if dir(obj) == dir(a_class):
+        return True
+    return False
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """
+    empty class
+    """
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """method that validates value"""
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    class Rectangle that inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """initializes variables and methods"""
+#        self.integer_validator("width", self.__width)
+#        self.integer_validator("height", self.__height)
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
