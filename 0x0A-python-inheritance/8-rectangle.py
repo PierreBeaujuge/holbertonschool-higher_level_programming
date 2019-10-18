6984cb1afb1f2fcaed8e327b830bfb6978b45fdf
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-10-18 02:52:48 +0000

    run integer validator first in __init__

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -28,12 +28,10 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """initializes variables and methods"""
-#        self.integer_validator("width", self.__width)
-#        self.integer_validator("height", self.__height)
-        self.__width = width
-        self.__height = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
 #        self.integer_validator("width", self.__width)
 #        self.integer_validator("height", self.__height)
