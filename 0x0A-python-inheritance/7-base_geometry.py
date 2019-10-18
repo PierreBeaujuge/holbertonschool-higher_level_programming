6c062f73ec97f51c9d08d5eea09b6350299e96b5
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-10-18 00:56:47 +0000

    replaced isinstance() by type()

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,9 +15,11 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """method that validates value"""
-#        if not isinstance(value, int):
-#        if type(value) is not int:
-        if value.__class__ is not int:
+        if type(value) != int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
+
+#        if value.__class__ is not int:
+#        if not isinstance(value, int):
+#        if type(value) is not int:
