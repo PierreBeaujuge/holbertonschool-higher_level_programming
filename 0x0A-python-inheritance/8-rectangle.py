18cd02014e6048762492e36c52cb18b4058e48be
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-10-18 03:31:00 +0000

    import BaseGeometry instead

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,24 +2,23 @@
 """
 BaseGeometry module
 """
-# BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+# class BaseGeometry:
+#     """
+#     empty class
+#     """
 
-class BaseGeometry:
-    """
-    empty class
-    """
+#     def area(self):
+#         """raises an Exception"""
+#         raise Exception('area() is not implemented')
 
-    def area(self):
-        """raises an Exception"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """method that validates value"""
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+#     def integer_validator(self, name, value):
+#         """method that validates value"""
+#         if not isinstance(value, int) or isinstance(value, bool):
+#             raise TypeError('{} must be an integer'.format(name))
+#         if value <= 0:
+#             raise ValueError('{} must be greater than 0'.format(name))
 
 
 class Rectangle(BaseGeometry):
@@ -29,12 +28,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """initializes variables and methods"""
-#        self.integer_validator("width", width)
-#        self.integer_validator("height", height)
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-#        self.integer_validator("width", self.__width)
-#        self.integer_validator("height", self.__height)
