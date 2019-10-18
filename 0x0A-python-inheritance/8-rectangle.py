b7e16ae50a91dcf8c7174cdad76b7cd977518d99
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-10-18 03:13:21 +0000

    import BaseGeometry instead

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,23 +2,23 @@
 """
 BaseGeometry module
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+# class BaseGeometry:
+#     """
+#     empty class
+#     """
 
-class BaseGeometry:
-    """
-    empty class
-    """
+#     def area(self):
+#         """raises an Exception"""
+#         raise Exception('area() is not implemented')
 
-    def area(self):
-        """raises an Exception"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """method that validates value"""
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+#     def integer_validator(self, name, value):
+#         """method that validates value"""
+#         if not isinstance(value, int) or isinstance(value, bool):
+#             raise TypeError('{} must be an integer'.format(name))
+#         if value <= 0:
+#             raise ValueError('{} must be greater than 0'.format(name))
 
 
 class Rectangle(BaseGeometry):
