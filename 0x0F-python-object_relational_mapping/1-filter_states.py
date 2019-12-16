29c2180f1201180f33fac8efd8ff21db9147b8f4
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-12-16 06:13:41 +0000

    switched to RLIKE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            passwd=argv[2],
                            db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name RLIKE '^N' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
