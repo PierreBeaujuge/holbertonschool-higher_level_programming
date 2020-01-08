8f859687b0ad48a0350bc42178242d0075461a71
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-01-08 05:36:12 +0000

    input answers

---
+++ 0x10-python-network_0/101-post_json.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Bash script that takes in a URL, sends a JSON POST request to the URL
+# and displays the body of the response
+curl -X POST $1 -H "Content-Type: application/json" -d @$2
