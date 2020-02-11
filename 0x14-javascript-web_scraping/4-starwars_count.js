7b05b20b8b4120de82de8213d0bdf52fdc757cc5
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-02-11 21:43:08 +0000

    added if condition on results

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,12 @@ const myArray = process.argv.slice(2);
 const request = require('request');
 
 request(myArray[0], function (error, response, body) {
-  if (error) console.error('error:', error); // Print the error if one occurred
-  const results = JSON.parse(body).results;
-  let count = 0;
-  results.forEach(dict => { if (dict.characters.includes('https://swapi.co/api/people/18/')) { count++; } });
-  console.log(count);
+  if (error) { console.log(error); } else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+    if (results && results.length >= 1) {
+      results.forEach(dict => { if (dict.characters.includes('https://swapi.co/api/people/18/')) { count++; } });
+    }
+    console.log(count);
+  }
 });
