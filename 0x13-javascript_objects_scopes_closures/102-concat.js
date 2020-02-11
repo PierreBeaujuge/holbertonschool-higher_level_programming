5360ce829dd1e2cc426d3e286e4200783aa1902d
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-02-11 06:00:41 +0000

    fixed semistandard

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,17 +2,17 @@
 
 const fs = require('fs');
 
-let myArray = process.argv.slice(2);
+const myArray = process.argv.slice(2);
 
-fs.readFile(myArray[0], (err, data_to_append) => {
+fs.readFile(myArray[0], (err, data) => {
+  if (err) throw err;
+  fs.appendFile(myArray[2], data, (err) => {
     if (err) throw err;
-    fs.appendFile(myArray[2], data_to_append, (err) => {
-	if (err) throw err;
-    });
+  });
 });
-fs.readFile(myArray[1], (err, data_to_append) => {
+fs.readFile(myArray[1], (err, data) => {
+  if (err) throw err;
+  fs.appendFile(myArray[2], data, (err) => {
     if (err) throw err;
-    fs.appendFile(myArray[2], data_to_append, (err) => {
-	if (err) throw err;
-    });
+  });
 });
